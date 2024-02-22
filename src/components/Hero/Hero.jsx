0852b5f2b0f7f6af84e7c805220bd7546e4afa2c
Imtious-Coder <iarafat386@gmail.com>
2024-02-22 13:48:46 +0600

    Header and Hero section updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Video from "../../Assets/video/intro.mp4";
+import HeroImage from "../../Assets/images/HOME/Hero.png";
 import "./Hero.scss";
 
 const Hero = () => {
@@ -7,24 +7,25 @@ const Hero = () => {
     <>
       <section className="hero-wrapper base-gradient-background py-5">
         <div className="container-lg py-md-5">
-          <div className="row align-items-center">
+          <div className="row align-items-center pt-5">
             <div className="col-12 col-md-6 ps-2">
               <div>
-                <h1 className="mb-4 text-center text-sm-start">
-                  Advancing the Effectiveness of Businesses
+                <h1 className="big-heading mb-4 text-center text-sm-start">
+                  Revolutionizing Operational Efficiency through AI Innovation
                 </h1>
                 <h4 className="text-center text-sm-start">
-                  AI-Powered Voice Processing, & Language Processing enabling
-                  companies to know customers better and provide the best
-                  possible services to them.
+                  Unlocking Customer Understanding and Service Excellence with
+                  Cutting-Edge AI-Powered Voice and Language Processing
+                  Solutions.
                 </h4>
               </div>
             </div>
             <div className="col-12 col-md-6 mt-sm-5 mt-md-0">
               <div className="mx-auto">
-                <video className="hero-video" autoPlay muted controls>
+                <img className="hero-image" src={HeroImage} alt="Hero_Image" />
+                {/* <video className="hero-video" autoPlay muted controls>
                   <source src={Video} type="video/mp4" />
-                </video>
+                </video> */}
               </div>
             </div>
           </div>
